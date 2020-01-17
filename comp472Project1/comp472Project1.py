d7177705c044ff1b1708347d4a0042dcf2ceb698
Jason Brennan <brenjaso@gmail.com>
2020-01-17 01:20:55 -0500

    Creation of State and Node classes, read file, starting initialize methods

--- comp472Project1/comp472Project1.py
+++ comp472Project1/comp472Project1.py
@@ -1,1 +1,36 @@
+import sys
+import numpy as np
 
+class State:
+    def __init__(self, M):
+        self.state = M
+
+class Node:
+    def __init__(self, p, s, d, c):
+        self.parent = p
+        self.state = s
+        self.depth = d
+        self.cost = c
+
+fileName = sys.argv[1]
+puzzleData = list()
+with open(str(fileName)) as file:
+    puzzleList = f.readlines().split()
+
+def initialize(p):
+    size = p[0]
+    maxDepth = p[1]
+    maxLength = p[2]
+    initialState = p[3]
+
+    index = 0
+    global M
+    M = np.empty([size, size], dtype=int)
+    for i in M:
+        for j in M[i]:
+            M[i,j] = initialState[index]
+            index += 1
+
+for p in puzzleList:
+    initialize(p)
+    solve(p)
